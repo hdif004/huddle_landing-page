23bbf86ada7c6fa2f2bd57ae1c3a72f92dfac397
Hudayfa Koujdal <102149280+hdif004@users.noreply.github.com>
2023-08-29 11:00:00 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'magenta': '#e882e8'
+      }
+    },
+  },
+  plugins: [],
+}
+
